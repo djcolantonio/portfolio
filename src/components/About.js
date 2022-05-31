3e9a8e5ee8bc01cd4c9429714efa8bc10cd38107
Daniel Colantonio <danielcolantonio@Dans-Mac.fios-router.home>
2022-05-30 21:56:39 -0400

    First commit

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,12 @@ const About = () => {
              I am currently a Partner Integration Analyst onboarding lenders to the software via backend XML configurations
              and deployments while managing multiple projects at once. I work directly with customers testing in QA and production
               to ensure connection between systems is successful. Over the past 2 years, I have become very interested in the Web3, Blockchain and Crypto space and
-              am I am currently enhancing my skills to become a Solidity/Blockchain developer. I believe smart contracts will change every
+              am I am currently enhancing my skills to become more knowledgeable and to become a Solidity/Blockchain developer. I believe smart contracts will change every
               industry and will make everyday routines more efficient and decentralized giving more power to the individual. Below is some more information about me and other web-development projects I worked on. I am profificient in HTML/CSS
               and I am learning more about React and Next.JS as I build projects based around Solidity.
             </p>
+            <br></br>
+            <p>I am currently working on a Web3 project that is a demo marketplace to auction real estate foreclosures with Next.js and Solidity.</p>
         </section>
     );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ import compound from '../assets/compound.png';
 import aave from '../assets/aave.png';
 import colantonio from '../assets/colantonio.png'
 import sh from '../assets/sh.png'
+import sh2 from '../assets/sh1.png'
 
 const Projects = () => {
     return (
@@ -14,8 +15,8 @@ const Projects = () => {
 
                 <div className="projects__card">
                     <h3>Colantonio Contracting</h3>
-                    <img src={colantonio} alt="Uniswap Swap Page" />
-                    <p>A simple site I developed, deployed and maintain on cPanel. I used my background of HTML and CSS for this project.
+                    <div className='proj'><img src={colantonio} alt="Uniswap Swap Page" /></div>
+                    <p className='info'>A simple site I developed, deployed and maintain on cPanel. I used my background of HTML and CSS for this project.
                     </p>
 
                     <a href="https://colantoniocandm.com/" target="_blank" className="button">Site</a>
@@ -24,8 +25,8 @@ const Projects = () => {
 
                 <div className="projects__card">
                     <h3>Safe Haven Sober Living</h3>
-                    <img src={sh} alt="sh" />
-                    <p>Another site made for a family business, but this was made via Google Sites. I am thinking about giving it an update soon using React.
+                    <div className='proj'><img src={sh} className='proj1' alt="sh" /></div>
+                    <p className='info'>Another site made for a family business, but this was made via Google Sites. I am thinking about giving it an update soon using React.
                     <br></br>
                     <br></br>
                     <br></br>
@@ -36,17 +37,22 @@ const Projects = () => {
                     {/* <a href="https://github.com/compound-finance/compound-protocol" target="_blank" className="button">Code</a> */}
                 </div>
 
-                {/* <div className="projects__card">
-                    <h3>Aave</h3>
-                    <img src={aave} alt="Aave Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                <div className="projects__card">
+                    <h3>Safe Haven Sober Living Update</h3>
+                    <div className='proj'><img src={sh2} className='proj1' alt="sh2" /></div>
+                    <p className='info'>I updated previous site with Next.js, HTML and CSS and deployed it to Vercel via Git command line. 
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    
+                    
+                   
+                  
                     </p>
 
-                    <a href="https://aave.com/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/aave/aave-v3-core" target="_blank" className="button">Code</a>
-                </div> */}
+                    <a href="https://safehaven-djcolantonio.vercel.app/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/djcolantonio/safehaven" target="_blank" className="button">Code</a>
+                </div>
             </div>
         </section>
     );
